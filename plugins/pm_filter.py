d7a182f79a9fcc33646b6736608787f12f5ce50a
mohamedimrans <96737338+mohamedimrans@users.noreply.github.com>
2022-02-02 09:23:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -806,7 +806,7 @@ async def advantage_spell_chok(msg):
     g_s += await search_gagala(msg.text)
     gs_parsed = []
     if not g_s:
-        k = await msg.reply("I couldn't find any movie in that name.")
+        k = await msg.reply("I couldn't find any movie in that name.If you didn't get movie tag @admin with movie name.")
         await asyncio.sleep(8)
         await k.delete()
         return
@@ -835,7 +835,7 @@ async def advantage_spell_chok(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist))  # removing duplicates
     if not movielist:
-        k = await msg.reply("I couldn't find anything related to that 👇 இதில் ஏதாவதா ?. Check your spelling")
+        k = await msg.reply("I couldn't find anything related to that. Check your spelling. If you didn't get movie tag @admin with movie name")
         await asyncio.sleep(8)
         await k.delete()
         return
